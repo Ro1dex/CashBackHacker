e8f617a7d5db4a6a2d24c18d7fdc1d7fc86413a9
Ro1dex <sanhcez58@gmail.com>
2023-06-22 18:54:29 +0200

    testingComplete

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,8 +2,32 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
-@Test
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+ @Test
+    public void shouldGivePercent (){
+     CashbackHackService service = new CashbackHackService();
+     int amount = 500;
+     int exp = 500;
+     int act = service.remain(amount);
+     assertEquals (act, exp);
 
 
+     amount = 1000;
+     exp = 0;
+     act = service.remain(amount);
+     assertEquals(act, exp);
+
+     amount = 1500;
+     exp = 500;
+     act = service.remain(amount);
+     assertEquals(act,exp);
+
+     amount = 2000;
+     exp = 0;
+     act = service.remain(amount);
+     assertEquals(act,exp);
+
+ }
 }
